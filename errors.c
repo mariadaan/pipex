ddb2101e4468fb6d88f4c2c6657ea54255553817
Maria Daan <mdaan@f1r3s9.codam.nl>
2021-10-06 20:10:14 +0200

    debug functions

--- errors.c
+++ errors.c
@@ -13,3 +13,19 @@ void	error_msg(int error_code, char *msg)
 	write(STDERR_FILENO, "\n", 1);
 	exit(error_code);
 }
+
+void print_info(t_args *args)
+{
+	printstr("file in ", args->file_in);
+	printf("--------------------------------------------\n");
+	printstr("file out", args->file_out);
+	printf("--------------------------------------------\n");
+	printstr("command 1", (args->full_cmd_1)[0]);
+	printf("--------------------------------------------\n");
+	printstr("command 2", (args->full_cmd_2)[0]);
+	printf("--------------------------------------------\n");
+	printstr("path 1", args->path_cmd_1);
+	printf("--------------------------------------------\n");
+	printstr("path 2", args->path_cmd_2);
+	printf("--------------------------------------------\n");
+}
