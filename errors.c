e520f157926722fa51c8836721a11ee19e0dfc72
Maria Daan <mariadaan@gmail.com>
2021-10-12 17:00:16 +0200

    debug stuff removed

--- errors.c
+++ errors.c
@@ -19,19 +19,3 @@ int	double_error_msg(char *msg, char *name, int error_code)
 	ft_putendl_fd(name, STDERR_FILENO);
 	return (error_code);
 }
-
-void print_info(t_args *args)
-{
-	printstr("file in ", args->file_in);
-	printf("--------------------------------------------\n");
-	printstr("file out", args->file_out);
-	printf("--------------------------------------------\n");
-	printstr("command 1", (args->full_cmd_1)[0]);
-	printf("--------------------------------------------\n");
-	printstr("command 2", (args->full_cmd_2)[0]);
-	printf("--------------------------------------------\n");
-	printstr("path 1", args->path_cmd_1);
-	printf("--------------------------------------------\n");
-	printstr("path 2", args->path_cmd_2);
-	printf("--------------------------------------------\n");
-}
